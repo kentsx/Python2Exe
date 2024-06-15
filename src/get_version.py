511d6aa84833e632a002420e26658e125498ba05
kentsx <kentisxie@126.com>
2024-06-16 00:13:24 +0800

    原来是action中少给了env参数

--- src/get_version.py
+++ src/get_version.py
@@ -1,16 +1,16 @@
 import importlib.util
 from actions import *
-import os
+# import os
 
 ## Version File Dir
-# version_file = env('version_file_path')
-version_file = 'version.py'  #强制给一个路径
-print('='*30)
-print(os.environ.get('VERSION_FILE_PATH'))
-print(os.environ.get('INPUTS'))
-print('='*30)
-print(version_file)
-print('='*30)
+version_file = env('version_file_path')
+# version_file = 'version.py'  #强制给一个路径
+# print('='*30)
+# print(os.environ.get('VERSION_FILE_PATH'))
+# print(os.environ.get('INPUTS'))
+# print('='*30)
+# print(version_file)
+# print('='*30)
 
 
 
@@ -18,7 +18,9 @@ print('='*30)
 spec = importlib.util.spec_from_file_location('version',version_file)  # This 'version' name seems no use
 module = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(module)
-print(module.VER)
+print('='*30)
+print('Release Tag: ', module.VER)
+print('='*30)
 set_output('version', module.VER)
 
 
