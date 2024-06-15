27e1e6d85a92058cf8b5c1478a92f4bc976831a7
kentsx <kentisxie@126.com>
2024-06-15 23:43:22 +0800

    报错是因为缺少库么？

--- src/get_version.py
+++ src/get_version.py
@@ -1,22 +1,21 @@
 import importlib.util
 from actions import *
+import os
 
 ## Version File Dir
 version_file = env('version_file_path')
+print('This is a test string')
 print(version_file)
+print('END of test String')
 
-# version_file = 'D:/Coding/IBD-Tool/version.py'
-# Version File Name
-# file_dir_w_name, file_ext = os.path.splitext(version_file)
-# file_dir, file_name = os.path.split(file_dir_w_name)
 
 
 # Load Module
 spec = importlib.util.spec_from_file_location('version',version_file)  # This 'version' name seems no use
 module = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(module)
-# print(type(module.VER))
-set_output('version', module.VER)
+print(module.VER)
+# set_output('version', module.VER)
 
 
 """
